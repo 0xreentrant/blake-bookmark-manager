775117dbe0e611bd0045da2a62f634c993f975ed
0xreentrant <0x7e1e@protonmail.com>
2024-05-24 22:45:11 -0600

    removed comments

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import "../styles/globals.css";
 
 export default function RootLayout({
-  // Layouts must accept a children prop.
-  //   // This will be populated with nested layouts or pages
   children,
 }: {
   children: React.ReactNode;
