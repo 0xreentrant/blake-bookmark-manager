3ee5bb9cb88fab1898b7fd6811c2db25e0156779
0xreentrant <0x7e1e@protonmail.com>
2024-06-02 08:27:14 -0600

    Geist fonts

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,7 +1,8 @@
-import '../../styles/globals.css'
+import { GeistSans } from "geist/font/sans";
+import "../../styles/globals.css";
 export default function Layout({ children }) {
   return (
-    <html>
+    <html className={GeistSans.className}>
       <body>{children}</body>
     </html>
   );
