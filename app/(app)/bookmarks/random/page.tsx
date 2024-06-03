252de91d11fbe09bf9776c7064315e8d87a30aa5
0xreentrant <0x7e1e@protonmail.com>
2024-06-02 19:32:59 -0600

    fix: lists work in random again

--- app/(app)/bookmarks/random/page.tsx
+++ app/(app)/bookmarks/random/page.tsx
@@ -3,34 +3,36 @@
 import { eq, desc, sql } from "drizzle-orm";
 import { db, dbNew } from "@/db";
 import { Bookmarks } from "@/components/Bookmarks";
-import { bookmarks } from "@/schema";
+import { bookmarks, lists, bookmarksToLists } from "@/schema";
 import { Nothing } from "@/components/DefaultViews/Nothing";
 
 export default async function Page() {
-  const random = dbNew
-    .select()
-    .from(bookmarks)
-    .where(eq(bookmarks.archived, 0))
-    .orderBy(sql`RANDOM()`)
-    .limit(10)
-    .as("random");
-
-  const list = await dbNew.select().from(random).orderBy(desc(random.date));
+  const list = await dbNew.query.bookmarks
+    .findMany({
+      with: { bookmarksToLists: { with: { list: true } } },
+      where: eq(bookmarks.archived, 0),
+      orderBy: [sql`RANDOM()`],
+      limit: 10,
+    })
+    .then((data) => {
+      // sort descending
+      return data.sort((a, b) => Number(b.date) - Number(a.date));
+    });
 
   const allLists = await dbNew.query.lists.findMany();
 
-  if (!list.length) {
+  if (!list?.length) {
     return <h1>Nothing Here!</h1>;
   }
 
   return (
- <>
+    <>
       <h1>Random bookmarks</h1>
       {list?.length ? (
- <Bookmarks bookmarks={list} allLists={allLists} />
- ) : (
- <Nothing />
- )}
+        <Bookmarks bookmarks={list} allLists={allLists} />
+      ) : (
+        <Nothing />
+      )}
     </>
   );
 }
