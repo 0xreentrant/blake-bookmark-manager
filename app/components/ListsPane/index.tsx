639a3c10582d0e98f11a36bddd533826aeea209a
0xreentrant <0x7e1e@protonmail.com>
2024-06-10 10:24:38 -0600

    fix: nav closes when new list created

--- app/components/ListsPane/index.tsx
+++ app/components/ListsPane/index.tsx
@@ -22,6 +22,7 @@ export function ListsPane({ lists, totalBookmarks, handleNavSelection }) {
     if (!newListData) {
       return;
     }
+handleNavSelection()
     const id = newListData.id;
     router.push(`/bookmarks/list/${id}`);
   }, [newListData]);
