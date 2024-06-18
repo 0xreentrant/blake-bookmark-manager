24294fd1d2a6d065b5a3d825e11dd47a459156d5
0xreentrant <0x7e1e@protonmail.com>
2024-06-17 21:35:59 -0600

    fix: no more dependencies on uk-icon

--- app/components/Icon/index.tsx
+++
@@ -1,7 +0,0 @@
-import React, { forwardRef } from "react";
-import dynamic from "next/dynamic";
-const UKIcon = dynamic(() => import("./UKIcon"), { ssr: false });
-const Icon = function (props) {
-  return <UKIcon {...props} />;
-};
-export { Icon };
