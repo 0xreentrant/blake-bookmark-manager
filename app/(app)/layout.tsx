655cacb90f7fa76dcbc64aac9f85541ab252daad
0xreentrant <0x7e1e@protonmail.com>
2024-07-15 12:57:23 -0600

    fix: prevent mysterious "pointer-events: none" from affecting the login page after logging out

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -2,8 +2,8 @@ import { GeistSans } from "geist/font/sans";
 import "../../styles/globals.css";
 export default function Layout({ children }) {
   return (
-    <html className={`${GeistSans.className}`}>
-      <body>{children}</body>
+    <html className={` ${GeistSans.className}`}>
+      <body className="!pointer-events-auto">{children}</body>
     </html>
   );
 }
