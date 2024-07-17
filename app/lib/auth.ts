9f84a1769d4faa57481b045e1971eab8dd527ab5
0xreentrant <0x7e1e@protonmail.com>
2024-07-17 12:24:04 -0600

    fix: add back missing types after git revert

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -40,45 +40,57 @@ interface DatabaseUserAttributes {
   family_name: string;
 }
 
+export type BlakeUser = {
+  username: string;
+  givenName: string;
+  familyName: string;
+  avatar: string;
+};
+
+export type GoogleUser = {
+  picture: string;
+  email: string;
+  given_name: string;
+  family_name: string;
+};
+
 export const google = new Google(
   process.env.GOOGLE_CLIENT_ID!,
   process.env.GOOGLE_CLIENT_SECRET!,
   process.env.GOOGLE_CLIENT_CALLBACK!
 );
 
-export type UserCookie = { user: User; session: Session } | { user: null; session: null }
-export const validateRequest = cache(
-  async (): Promise<
-   UserCookie 
-  > => {
-    const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
-    if (!sessionId) {
-      return {
-        user: null,
-        session: null,
-      };
-    }
-
-    const result = await lucia.validateSession(sessionId);
-    // next.js throws when you attempt to set cookie when rendering page
-    try {
-      if (result.session && result.session.fresh) {
-        const sessionCookie = lucia.createSessionCookie(result.session.id);
-        cookies().set(
-          sessionCookie.name,
-          sessionCookie.value,
-          sessionCookie.attributes
-        );
-      }
-      if (!result.session) {
-        const sessionCookie = lucia.createBlankSessionCookie();
-        cookies().set(
-          sessionCookie.name,
-          sessionCookie.value,
-          sessionCookie.attributes
-        );
-      }
-    } catch {}
-    return result;
+export type UserCookie =
+  | { user: User; session: Session }
+  | { user: null; session: null };
+export const validateRequest = cache(async (): Promise<UserCookie> => {
+  const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+  if (!sessionId) {
+    return {
+      user: null,
+      session: null,
+    };
   }
-);
+
+  const result = await lucia.validateSession(sessionId);
+  // next.js throws when you attempt to set cookie when rendering page
+  try {
+    if (result.session && result.session.fresh) {
+      const sessionCookie = lucia.createSessionCookie(result.session.id);
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      );
+    }
+    if (!result.session) {
+      const sessionCookie = lucia.createBlankSessionCookie();
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      );
+    }
+  } catch {}
+  return result;
+});
