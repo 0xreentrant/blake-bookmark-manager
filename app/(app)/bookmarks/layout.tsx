c37be5646162e77941f02f419c08002095ae9534
0xreentrant <0x7e1e@protonmail.com>
2024-07-17 23:50:56 -0600

    fix: correct object property when saving user id

--- app/(app)/bookmarks/layout.tsx
+++ app/(app)/bookmarks/layout.tsx
@@ -27,7 +27,7 @@ export default async function BookmarksLayout({ children }) {
         throw new Error("User not found");
       }
       return {
-        userId: userCookieId,
+        userId: userCookieId.id,
         username: user.googleId,
         givenName: user.givenName,
         familyName: user.familyName,
