8cf5ee925a17383b2bada38ac5a2bad817c8a785
0xreentrant <0x7e1e@protonmail.com>
2024-09-01 12:44:00 -0600

    fix: prevent page from overflowing

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -2,7 +2,7 @@ import { GeistSans } from "geist/font/sans";
 import "../../styles/globals.css";
 export default function Layout({ children }) {
   return (
-    <html className={` ${GeistSans.className}`}>
+    <html className={` ${GeistSans.className} overflow-hidden`}>
       <body className="!pointer-events-auto">{children}</body>
     </html>
   );
