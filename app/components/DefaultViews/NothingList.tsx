04ec14f57bf9d267051724468b05330e0f967a09
0xreentrant <0x7e1e@protonmail.com>
2024-09-17 10:27:23 -0600

    fix: fixed pointer events so this and parent elements can be clicked

--- app/components/DefaultViews/NothingList.tsx
+++ app/components/DefaultViews/NothingList.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 export function NothingList() {
   return (
-    <div className="[z-index:-1] absolute w-full h-full flex items-center justify-center flex-col">
+    <div className="absolute pointer-events-none w-full h-full flex items-center justify-center flex-col">
       <h1>Nothing Here!</h1>
-      <p>
+      <p className="pointer-events-auto">
         Add bookmarks from{" "}
         <Link href="/bookmarks/all" className="underline">
           All Bookmarks
